1a453fb472a45cc0093a93420edf3fb9ce224753
irino <irino@wakayama-u.ac.jp>
2022-01-26 13:08:04 +0900

    function StartupGCFB.m

--- GCFBv231/StartupGCFB.m
+++ GCFBv231/StartupGCFB.m
@@ -3,9 +3,16 @@
 %      IRINO T.
 %      Created:    14 Sep 2021
 %      Modified:   14 Sep 2021
+%      Modified:   22 Jan 2022  % defined as a function not to overwrite  DirProg
 %
 %      Setting path at least once before starting GCFB programs
 %   
-DirProg = fileparts(which(mfilename));
+function DirProg = StartupGCFB
+
+DirProg = fileparts(which(mfilename)); % Directory of this program
 addpath([DirProg '/Tool/']);    
 
+% mfilename
+% DirProg
+
+end
